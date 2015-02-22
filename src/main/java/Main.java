209924386b15795955db06337259a398d56787bc
jaimiexie <jaimiexie@dn0a233317.sunet>
2015-02-21 16:36:49 -0800

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,92 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.twilio.sdk.verbs.Message;
+import com.twilio.sdk.verbs.TwiMLException;
+import com.twilio.sdk.verbs.TwiMLResponse;
+
+public class Main extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		TwiMLResponse twiml = new TwiMLResponse();
+		Message message = new Message("Hello, Jaimie");
+		try {
+			twiml.append(message);
+		} catch (TwiMLException e) {
+			e.printStackTrace();
+		}
+
+		resp.setContentType("application/xml");
+		resp.getWriter().print(twiml.toXML());
+
+	}
+
+	private void showHome(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.getWriter().print("Hello from Java!");
+	}
+
+	private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		Connection connection = null;
+		try {
+			connection = getConnection();
+
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+			String out = "Hello!\n";
+			while (rs.next()) {
+				out += "Read from DB: " + rs.getTimestamp("tick") + "\n";
+			}
+
+			resp.getWriter().print(out);
+		} catch (Exception e) {
+			resp.getWriter().print("There was an error: " + e.getMessage());
+		} finally {
+			if (connection != null)
+				try {
+					connection.close();
+				} catch (SQLException e) {
+				}
+		}
+	}
+
+	private Connection getConnection() throws URISyntaxException, SQLException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		int port = dbUri.getPort();
+
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port
+				+ dbUri.getPath();
+
+		return DriverManager.getConnection(dbUrl, username, password);
+	}
+
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+		ServletContextHandler context = new ServletContextHandler(
+				ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		server.setHandler(context);
+		context.addServlet(new ServletHolder(new Main()), "/*");
+		server.start();
+		server.join();
+	}
+}
